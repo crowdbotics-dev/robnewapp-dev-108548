f4a5e24f5158c0706ed4a1a7ffa02eb4f23d9ce5
Robert So <robkso@gmail.com>
2023-08-21 11:53:57 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled7/index.js
+++ screens/Untitled7/index.js
@@ -8,7 +8,7 @@ const Untitled7 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}><View style={styles.hMkGyaOe}></View></ScrollView>
+    }}><View style={styles.hMkGyaOe}></View><View style={styles.JFDfKApC}><View style={styles.kuqKXzOy}></View><View style={styles.QeIybPbE}></View></View></ScrollView>
     </SafeAreaView>;
 };
 
@@ -16,6 +16,27 @@ const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
   },
-  hMkGyaOe: {}
+  hMkGyaOe: {},
+  JFDfKApC: {
+    height: 501,
+    width: 346,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  kuqKXzOy: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#e42020",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  QeIybPbE: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#60f431",
+    borderRadius: 0,
+    color: "#777777"
+  }
 });
 export default Untitled7;
